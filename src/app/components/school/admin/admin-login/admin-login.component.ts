a2f8d202e39a58d1cf03516f15ac20a91c2a38c2
Don Thomas <donthomas20399@gmail.com>
2024-02-16 22:07:02 +0530

    started on school admin login

--- src/app/components/school/admin/admin-login/admin-login.component.ts
+++ src/app/components/school/admin/admin-login/admin-login.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { SchoolAdminService } from '../../services/school-admin.service';
 
 @Component({
   selector: 'app-admin-login',
   templateUrl: './admin-login.component.html',
   styleUrls: ['./admin-login.component.css']
 })
-export class AdminLoginComponent {
+export class AdminLoginComponent implements OnInit {
+  isSubmitted:boolean = false
+  form!:FormGroup
+  constructor(
+    private readonly formBuilder:FormBuilder,
+    private schoolAdminService:SchoolAdminService
+  ){}
+  ngOnInit(): void {
+    
+  }
+  submit(){
+
+  }
 
 }
