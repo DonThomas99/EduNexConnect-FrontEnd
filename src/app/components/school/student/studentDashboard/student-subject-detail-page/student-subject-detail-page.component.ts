6feb78e113557a5458fc8e84add23d05bdcb783b
Don Thomas <donthomas20399@gmail.com>
2024-04-05 10:44:36 +0530

    feat(Implemented pagination for the stream section of student section)

--- src/app/components/school/student/studentDashboard/student-subject-detail-page/student-subject-detail-page.component.ts
+++ src/app/components/school/student/studentDashboard/student-subject-detail-page/student-subject-detail-page.component.ts
@@ -3,7 +3,7 @@ import { Store } from '@ngrx/store';
 import { pipe } from 'rxjs';
 import { selectSubjectId, selectTenantId } from 'src/app/states/school/school.selector';
 import { StudentService } from '../../../services/student.service';
-import { IMatAsmnt } from 'src/app/Models/material';
+import { IMat, IMatAsmnt } from 'src/app/Models/material';
 import { DatePipe } from '@angular/common';
 
 @Component({
@@ -12,6 +12,11 @@ import { DatePipe } from '@angular/common';
   styleUrls: ['./student-subject-detail-page.component.css']
 })
 export class StudentSubjectDetailPageComponent implements OnInit {
+currentPage:number = 1
+itemsPerPage:number = 4
+totalPages:number =0
+totalItems:number =0
+selectedOption:string = 'Materials';
 tenantId!:string
 subjectId!:string
 uploadsArray!:IMatAsmnt[]
@@ -19,9 +24,39 @@ uploadsArray!:IMatAsmnt[]
     private datePipe:DatePipe,
     private readonly store : Store,
     private readonly studentService:StudentService
-  ){
+  ){}
 
-  }
+  onSelectionChange(option: string): void {
+    this.uploadsArray = [];
+    this.selectedOption = option;
+    this.fetchMaterialsOrAssignments(this.currentPage, this.itemsPerPage);
+ }
+
+ fetchMaterialsOrAssignments(page: number, limit: number): void {
+   
+   if (this.selectedOption === 'Materials') {
+     this.studentService.fetchMatAsnmt(this.tenantId, this.subjectId, page, limit).subscribe({
+       next: (res: IMat) => {
+         // console.log(res);
+         this.uploadsArray = res.Mat;
+         console.log('heow',this.uploadsArray);
+         
+        }
+      });
+    } else {
+      console.log('vannthitta',page,limit);
+      this.studentService.fetchAssignments(this.tenantId, this.subjectId, page, limit).subscribe({
+        next: (res: IMat) => {
+          console.log(res);
+          this.uploadsArray = res.Mat;
+        }
+      });
+    }
+    this.totalPages = Math.ceil(this.totalItems / this.itemsPerPage) + 1 ;
+    console.log(this.totalPages);
+    
+ }
+
   tenantId$ = this.store.select(pipe(selectTenantId))
 subjectId$ = this.store.select(pipe(selectSubjectId))
 
@@ -36,13 +71,34 @@ subjectId$ = this.store.select(pipe(selectSubjectId))
         this.subjectId = id.subjectId as unknown as string
       
       })
-      this.studentService.fetchMatAsnmt(this.tenantId,this.subjectId).subscribe({
-        next:(res:IMatAsmnt[])=>{
-          console.log(res);
-          this.uploadsArray = res
+      this.studentService.fetchMatAsnmt(this.tenantId,this.subjectId,this.currentPage,this.itemsPerPage).subscribe({
+        next:(res:IMat)=>{
+          console.log();
+          
+          this.uploadsArray = res.Mat
+          this.totalItems = res.count
+          console.log('eee',this.totalItems);
           
         }
       })
   }
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+       this.currentPage--;
+       this.fetchMaterialsOrAssignments(this.currentPage, this.itemsPerPage);
+    }
+   }
+   
+   nextPage(): void {
+    console.log('vaiee');
+    
+    if (this.currentPage < this.totalPages) {
+       this.currentPage++;
+       this.fetchMaterialsOrAssignments(this.currentPage, this.itemsPerPage)
+    }
+   }
+   
+   
 
 }
