c1c16af472f9226bc00b784e3281c3da7e478a1f
Don Thomas <donthomas20399@gmail.com>
2024-04-15 10:24:09 +0530

    feat(synopsis):Implemented feature for viewing the number of school students and teachers in the dashboard

--- src/app/components/school/admin/adminDashboard/dashboard/dashboard.component.ts
+++ src/app/components/school/admin/adminDashboard/dashboard/dashboard.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { pipe } from 'rxjs';
+import { selectTenantId } from 'src/app/states/school/school.selector';
+import { SchoolAdminService } from '../../../services/school-admin.service';
+import { summary } from 'src/app/Models/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  tenantId$ = this.store.select(pipe(selectTenantId))
+  tenantId!:string
+  teacherCount!:number
+  studentCount!:number
+
+  constructor(
+    private readonly store :Store,
+    private readonly adminService:SchoolAdminService,
+    private readonly router:Router
+  ){
+
+  }
+  ngOnInit(): void {    
+    this.tenantId$.subscribe((tenantId)=>{
+      this.tenantId = tenantId as unknown as string
+    })
+
+    this.adminService.fetchSummary(this.tenantId).subscribe({
+      next:(res:summary)=>{
+        console.log(res.studentsCount,res.teacherCount);
+        this.teacherCount= res.teacherCount
+        this.studentCount = res.studentsCount
+      }
+    })
+
+  }
+  teachers(){
+this.router.navigate(['/school/admin/dashboard/teachers'])
+  }
+  students(){
+    this.router.navigate(['/school/admin/dashboard/students'])
+  }
 
 }
