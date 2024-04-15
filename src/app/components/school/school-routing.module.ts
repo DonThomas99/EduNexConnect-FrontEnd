f4a1bb482bf9b3596adc31a39dec9d8d3e102662
Don Thomas <donthomas20399@gmail.com>
2024-04-15 11:01:40 +0530

    feat(school Routing): Improved routing

--- src/app/components/school/school-routing.module.ts
+++ src/app/components/school/school-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SchoolhomeComponent } from './schoolhome/schoolhome.component';
+import { VideoClassComponent } from '../common/video-class/video-class.component';
 
 const routes: Routes = [
   {path:'',component:SchoolhomeComponent},
   {path:'admin',loadChildren:() => import('./admin/admin.module').then(m=>m.AdminModule)},
   {path:'teacher',loadChildren:() => import('./teacher/teacher.module').then(m=>m.TeacherModule)},
-  {path:'student',loadChildren:() => import('./student/student.module').then(m=>m.StudentModule)}
-
-
+  {path:'student',loadChildren:() => import('./student/student.module').then(m=>m.StudentModule)},
+  {path:'v-class/:roomId',component:VideoClassComponent}
 
 ];
 
