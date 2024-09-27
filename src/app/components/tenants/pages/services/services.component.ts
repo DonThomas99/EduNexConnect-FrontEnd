9cb7af5d68cc7e027c99661aed0b6ae9a64c60ee
Don Thomas <donthomas20399@gmail.com>
2024-09-27 11:24:05 +0530

    Feat(plans):Implemented a new page to view all the existing subscription plans

--- src/app/components/tenants/pages/services/services.component.ts
+++ src/app/components/tenants/pages/services/services.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IPlan } from 'src/app/Models/tenants';
+import { SuperAdminService } from 'src/app/services/super-admin.service';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.css']
 })
-export class ServicesComponent {
-
+export class ServicesComponent implements OnInit {
+  plans!:IPlan[]
+  planStatus!:boolean
+  constructor(
+    private readonly superAdminService:SuperAdminService
+  ){}
+  ngOnInit(): void {
+    this.planStatus = false
+    this.fetchPlans()
+  }
+  fetchPlans(){
+    this.superAdminService.fetchPlans().subscribe({
+      next:(res)=>{
+        this.plans = res.data || []
+        if(this.plans.length >0){
+          this.planStatus = true
+        }
+      },
+      error:(err)=>{
+        console.log('Error Fetching Plans');
+      }
+    })
+  }
 }
