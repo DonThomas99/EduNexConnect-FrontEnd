0e75e723384d5aedd23e12f6571e07caca2277c3
Don Thomas <donthomas20399@gmail.com>
2024-09-28 21:59:50 +0530

    fix(Tenant Login):fixed lgin management

--- src/app/components/tenants/pages/tenant-log-in/tenant-log-in.component.ts
+++ src/app/components/tenants/pages/tenant-log-in/tenant-log-in.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Store, select } from '@ngrx/store';
+import { ToastrService } from 'ngx-toastr';
 import { validateBytrimming } from 'src/app/helpers/validations';
 import { TenantService } from 'src/app/services/tenant.service';
 import { emailValidators, passwordValidators } from 'src/app/shared/validators';
@@ -21,7 +22,8 @@ export class TenantLogInComponent implements OnInit{
   private readonly formBuilder:FormBuilder,
   private readonly tenatnService:TenantService,
   private readonly router:Router,
-  private readonly store:Store
+  private readonly store:Store,
+  private readonly toastr:ToastrService
   ){}
   ngOnInit(): void {
     this.form = this.formBuilder.group({
@@ -36,14 +38,15 @@ export class TenantLogInComponent implements OnInit{
       this.tenatnService.login(data.email,data.password).subscribe({
         next:(res:any)=>{
           console.log(res, 'res from login');
-          
-          console.log('res saving on store');
           this.store.dispatch(saveTenantOnStore({tenantDetails: res.data.emailDb}))
           localStorage.setItem('tenantJwt',res.data.accessToken)
-          console.log('redirecting to  home');
-          
-          void this.router.navigate(['/tenant'])
-      }
+            this.toastr.success('Login Successfully')
+            void this.router.navigate(['/tenant'])
+          },
+          error:(err:any)=>{
+            this.toastr.error('Login Failed')
+            console.log(err)
+          }          
     })
   }
 }
