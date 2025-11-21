4c0db0b8d4b59dc0d5d8dff0fb1edcefc67f3c5f
Don Thomas <donthomas20399@gmail.com>
2025-11-22 01:25:07 +0530

    fix(school-admin):fixed some design issues in dashboard

--- src/app/components/school/admin/adminDashboard/side-nav/side-nav.component.ts
+++ src/app/components/school/admin/adminDashboard/side-nav/side-nav.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./side-nav.component.css']
 })
 export class SideNavComponent {
- 
+  adminNavLinks = [
+    { label: 'Overview', path: '/school/admin/dashboard' },
+    { label: 'Teachers', path: '/school/admin/dashboard/teachers' },
+    { label: 'Classes', path: '/school/admin/dashboard/subjects' },
+    { label: 'Students', path: '/school/admin/dashboard/students' },
+    { label: 'Sign-Out', path: '/school/admin' },
+  ];
 }
